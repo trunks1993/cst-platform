3aa5384395326d3545fcee8fdb4d733cb7c5f07a
Lesliey1nwk <l>
2019-10-15 17:18:33 +0800

    feat:新建弹窗，仪表盘样式修改

--- src/api/index.js
+++ src/api/index.js
@@ -20,3 +20,16 @@ export const getTableList = queryList => request({
   url: `/user/getTableList?current=${queryList.current}&pageSize=${queryList.pageSize}`,
   method: 'get'
 });
+
+export const getPublicTemp = params => request({
+  url: `/user/getTableList?current=${params.current}&pageSize=${params.pageSize}`,
+  method: 'get'
+});
+
+export const getStaticTemp = params => request({
+  url: `/v1/userConfigInfo/CusUserConfig/select?cucUserId=${params.cucUserId}`,
+  method: 'get',
+  params: {
+    token: params.token
+  }
+});
