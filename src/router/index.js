b4562790766898e78338d81e48d60b60422c110d
wangzhijian <604723443@qq.com>
2019-11-04 13:40:27 +0800

    feat: splite cs and cs

--- src/router/index.js
+++ src/router/index.js
@@ -6,19 +6,15 @@ import AuthRoute from './authRoute';
 import LoginPage from '@/pages/loginPage';
 import Layout from '@/pages/layout';
 import Page404 from '@/pages/page404';
-import Dashboard from '@/pages/dashboard';
-import Home from '@/pages/home';
 
 import Manufacturer from '@/pages/manufacturer';
-
-const isCs = (process.env.REACT_APP_ENV === 'cs');
 
 export default () => (
   <HashRouter>
     <Switch>
       <AuthRoute exact path="/login" authTo="/" component={LoginPage} />
       <AuthRoute path="/platForm" authTo="/login" component={Layout} />
-      <Redirect exact from="/" to={ isCs ? '/platForm/config' : '/platForm/home' } />
+      <Redirect exact from="/" to="/platForm/config" />
       <Route component={Page404} />
     </Switch>
   </HashRouter>
@@ -27,8 +23,7 @@ export default () => (
 export const RouteList = () => {
   return (
     <Switch>
-      { !isCs && <Route path="/platForm/home" component={Home} /> }
-      <Route path="/platForm/config" component={isCs ? Manufacturer : Dashboard} />
+      <Route path="/platForm/config" component={Manufacturer} />
       <Route component={Page404} />
     </Switch>
   );
